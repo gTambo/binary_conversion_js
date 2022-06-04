3b18d77ed4593a5367f1730d99ce93a634e7d34e
Greg Tambornino <gregtambornino@gmail.com>
2022-06-04 15:25:49 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Binary Converter</title>
+	<meta charset="UTF-8" />
+</head>
+
+<body>
+	<div id="app"></div>
+  <script src="src/index.js">
+	</script>
+</body>
+
+</html>
